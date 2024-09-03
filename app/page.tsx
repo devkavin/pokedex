e472cd88176545a855643c1b086f4847288200d1
devkavin <kavindra.senanayake@gmail.com>
2024-09-03 17:29:29 +0530

    Add floating animation to Pokedex title

--- app/page.tsx
+++ app/page.tsx
@@ -25,7 +25,7 @@ export default function Home() {
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-top p-24 text-black">
-      <p className="text-center text-5xl text-yellow-500">POKEDEX</p>
+      <p className="text-center text-5xl text-yellow-500 animate-float">POKEDEX</p>
       <FloatingInput onSearch={fetchPokemon} />
       <PokemonCard pokemonData={pokemonData} loading={loading} error={error} />
     </main>
